d907eb76518f241b7cb937b1619a3b1778e9a88c
monden-t <monden.t000@gmail.com>
2019-05-27 18:19:01 +0900

    add reposiroty test

---
+++ src/test/resouces/testdata.sql
@@ -0,0 +1,2 @@
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('junit@test.com','$2a$08$pMRANsr.lRvZUwpsHjHOIu5VbYXDg.v58RVdGavWa8tnfsfijr5li','junit太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
